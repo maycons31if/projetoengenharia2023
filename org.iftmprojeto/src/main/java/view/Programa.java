6929d866d86e3b77b8a5104028268c936f135771
Maycon <Maycon@DESKTOP-CDT9DB2>
2023-09-17 07:47:31 -0300

    A3-(Etapa 2)

--- org.iftmprojeto/src/main/java/view/Programa.java
+++ org.iftmprojeto/src/main/java/view/Programa.java
@@ -1,17 +1,20 @@
 package view;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
-import model.TelaLivro;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+import model.tblivro;
 
 public class Programa {
 	public static void main(String[] args) {
 	
-	TelaLivro v = new TelaLivro( "9788533302273", "Programacao Java", "2", "IFTM", "portugues", "1", "2023");
-	TelaLivro v1 = new TelaLivro( "0088533302278", "C++", "1", "educa", "ingles", "2", "1990");
-	TelaLivro v2 = new TelaLivro( "1288533302235", "Cobol", "4", "virtual", "portugues", "3", "1980");
+		tblivro v = new tblivro( "9788533302273", "Programacao Java", "2", "IFTM", "portugues", "1", "2023");
+		tblivro v1 = new tblivro( "0088533302278", "C++", "1", "educa", "ingles", "2", "1990");
+		tblivro v2 = new tblivro( "1288533302235", "Cobol", "4", "virtual", "portugues", "3", "1980");
 	
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory(library);
